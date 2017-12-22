d2f5ce899e991a2c408e9723f26fa0289dece588
kaushik yadav <kaushikyadav1999@gmail.com>
2017-12-22 23:14:41 +0530

    v3.0

--- app/src/main/java/com/android/paritosh/librarymanagement/AllLoginActivity.java
+++ app/src/main/java/com/android/paritosh/librarymanagement/AllLoginActivity.java
@@ -2,6 +2,7 @@ package com.android.paritosh.librarymanagement;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -53,6 +54,29 @@ public class AllLoginActivity extends AppCompatActivity implements View.OnClickL
         regis.setOnClickListener(this);
 
     }
+
+
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+
 
     public void books(View view) {
         // Create a new intent to open the {@link PhrasesActivity}
--- app/src/main/java/com/android/paritosh/librarymanagement/UserLoginActivity.java
+++ app/src/main/java/com/android/paritosh/librarymanagement/UserLoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,6 +79,29 @@ public class UserLoginActivity extends AppCompatActivity implements View.OnClick
         }
 
         email.setText("welcome " + displayName);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.log_out, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.logout_menu:
+
+
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(this, AllLoginActivity.class));
+
+        }
+        return true;
     }
 
     @Override
